61ac8f2c083cdd1d29e3ed819e430d432a3b3d91
Stefan Schuddinck <stefanschuddinck@hotmail.com>
2018-02-06 10:13:50 +0100

    project en aanspreken JSON

--- js/Profile.js
+++ js/Profile.js
@@ -3,40 +3,48 @@
 
 /*-------------------------------WINDOW.ONLOAD-------------------------------*/
 
-window.onload = function()
-    {
-    //----------DOM-REFENTIES------tip: gebruik hiervoor name!----------
-        var eFrmProfile = document.frmProfile;
-        var eFamilienaam = document.frmProfile.familienaam;
-        var eGeboortedatum= document.frmProfile.geboortedatum;
-        var eEmail = document.frmProfile.email;
-        var eNickname = documetn.frmProfile.nickname;
-        var eHaarkleur = document.frmProfile.haarkleur;
-        var eOogkleur = document.frmProfile.oogkleur;
+//window.onload = function()
+//    {
+//    //----------DOM-REFENTIES------tip: gebruik hiervoor name!----------
+//        var eFrmProfile = document.frmProfile;
+//        var eFamilienaam = document.frmProfile.familienaam;
+//        var eGeboortedatum= document.frmProfile.geboortedatum;
+//        var eEmail = document.frmProfile.email;
+//        var eNickname = documetn.frmProfile.nickname;
+//        var eHaarkleur = document.frmProfile.haarkleur;
+//        var eOogkleur = document.frmProfile.oogkleur;
 
     //----------HAARKLEUR INVLOEGEN-------------------------------------
+        var aKleurenHaren = [];
+        window.onload = function(){
+           aKleurenHaren = JSON.parse(aKleurenHaren);
+            alert("Hello wortld2");
+           alert(aKleurenHaren[0]);
+            
+        };
+        
         
 
-    //----------EVENT-HANDLERS------------------------------------------
-    eFrmVlucht.addEventListener('submit',function(event)
-        {
-        event.preventDefault();
-        var bValid = valideer(this);
-        if(bValid===true)this.submit();
-        });
-    eRetour.addEventListener('click',function()
-        {
-        vluchtType(this.value);
-        });
-    eEnkel.addEventListener('click',function()
-        {
-        vluchtType(this.value); 
-        });
-    eVolw.addEventListener('blur',passagierNamen);
-    eKind.addEventListener('blur',passagierNamen);
-    ePeut.addEventListener('blur',passagierNamen);
-
-    //----------GLOBAAL OBJECT met ALLE VALIDATIEREGELS------------------
-    oValidRules = 
+//    //----------EVENT-HANDLERS------------------------------------------
+//    eFrmVlucht.addEventListener('submit',function(event)
+//        {
+//        event.preventDefault();
+//        var bValid = valideer(this);
+//        if(bValid===true)this.submit();
+//        });
+//    eRetour.addEventListener('click',function()
+//        {
+//        vluchtType(this.value);
+//        });
+//    eEnkel.addEventListener('click',function()
+//        {
+//        vluchtType(this.value); 
+//        });
+//    eVolw.addEventListener('blur',passagierNamen);
+//    eKind.addEventListener('blur',passagierNamen);
+//    ePeut.addEventListener('blur',passagierNamen);
+//
+//    //----------GLOBAAL OBJECT met ALLE VALIDATIEREGELS------------------
+//    oValidRules = 
 
 
